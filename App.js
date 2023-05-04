e4d69f863f17d667b06df00b53549e3af7b2cd1a
SurajSingh7 <220suraj@gmail.com>
2023-05-04 16:31:43 +0530

    mini server

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
 import React  from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 
+const head1=React.createElement("h1",{id:"ti"},"hello1");
+const head2=React.createElement("h1",{id:"ti"},"hello2");
+const head3=React.createElement("h1",{id:"ti"},"hello3");
+console.log("jh");
+
+
+const container=React.createElement("div",{id:"pk"},[head2,head1,head3]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(container);
