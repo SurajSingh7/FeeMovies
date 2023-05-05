7ef53d7a0613456f80741e6cd2950dc73a8c1fb3
SurajSingh7 <220suraj@gmail.com>
2023-05-05 21:55:28 +0530

    jsx

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React  from "react";
 import ReactDOM from "react-dom/client";
 
-const head1=React.createElement("h1",{id:"ti"},"hello1");
-const head2=React.createElement("h1",{id:"ti"},"hello2");
-const head3=React.createElement("h1",{id:"ti"},"hello3");
-console.log("jh");
+const head1=React.createElement("h1",{id:"ti",key:"3"},"hello1");
+const head2=React.createElement("h1",{id:"ti",key:"1"},"hello2");
+const head3=React.createElement("h1",{id:"ti",key:"2"},"hello3");
 
 
-const container=React.createElement("div",{id:"pk"},[head2,head1,head3]);
+const container=React.createElement("div",{id:"pk"},[head1,head2,head3]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
+const head4=<h1 id="title" key="h2">hello 4</h1>;
 
-root.render(container);
+root.render(head4);
