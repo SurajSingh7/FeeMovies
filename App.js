6268c4c205e69f862dfc0bcd7b04c79fd1d96410
SurajSingh7 <220suraj@gmail.com>
2023-05-06 20:08:36 +0530

    start food app

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
 import React  from "react";
 import ReactDOM from "react-dom/client";
 
-const head1=React.createElement("h1",{id:"ti",key:"3"},"hello1");
-const head2=React.createElement("h1",{id:"ti",key:"1"},"hello2");
-const head3=React.createElement("h1",{id:"ti",key:"2"},"hello3");
+// const jsx=(
+//     <React.Fragment>
+//         <h1>hi1</h1>
+//         <h1>hi2</h1>
+//     </React.Fragment>
+// )
+
+// const jsx=(
+//     <>
+//         <h1>hi1</h1>
+//         <>
+//         <h1>hi2</h1>
+//         <h1>hi3</h1>
+//         </>
+       
+//     </>
+// )
+
+const Title =()=>(
+    <a href="/">
+     <img className="logo" alt="logo" src="https://media-cdn.tripadvisor.com/media/photo-s/1c/58/cd/b8/logo.jpg" />
+     </a>
+);
 
 
-const container=React.createElement("div",{id:"pk"},[head1,head2,head3]);
+const Header=()=>{
+    return (
+
+        <div className="header">
+            <Title/>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-
-const head4=<h1 id="title" key="h2">hello 4</h1>;
-
-root.render(head4);
+//  root.render(HeaderComponent());
+ root.render( <Header/>)
