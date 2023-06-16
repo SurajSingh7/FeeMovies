2d513dacb97b78e3f04a429dc4f88dd256a75ae8
SurajSingh7 <220suraj@gmail.com>
2023-06-17 00:53:57 +0530

    useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,55 +1,49 @@
 // Body part- 2
-
+import { restaurantList } from "../utils/constants";
+import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
-const BurgerKing={
-    name:"Burger King",
-    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/768px-Burger_King_logo_%281999%29.svg.png",
-    cusines:["Burger","American"],
-    rating:"4.2"
-}
 
-
-
-const RestrauntCard=()=>{
-    return (
-        <div className="card">
-
-            <img src={BurgerKing.image}/>
-            <h2>{BurgerKing.name}</h2>
-            <h3>{BurgerKing.cusines.join(",   ")}</h3>
-            <h4>{BurgerKing.rating} stars </h4>
-
-        </div>
-
+function filterData(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) =>
+      restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
     );
-};
+    return filterData;
+  }
+
 
 const Body=()=>{
-        // let searchTxt ="hi";
-        let [searchTxt,st]=useState("hik");
+
+        const [restaurants,setrestaurants]=useState(restaurantList);
+        //  let searchTxt ="hi";
+        const [searchText,setSearchText]=useState();
+
 
     return (
        
-        
        <>
         <div className="search-container">
             <input type="text" className="search-input" placeholder="Seakrch" 
-            value={searchTxt} onChange={(e)=>{st(e.target.value) }} />
+             value={searchText} onChange={(e)=>{ setSearchText(e.target.value); }} />
 
-            <button className="search-btn">Search</button>
+            <button className="search-btn" onClick={()=>{  
+              // need to filter the data 
+              const data =filterData(searchText,restaurants);
+              // update the state -restaurants
+              setrestaurants(data);
+            }}>
+            Search</button>
         </div>
 
         <div className="restaurant-list">
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
-            <RestrauntCard/>
+            {
+                restaurants.map((restraunt) =>{
+                    return(
+                          <RestaurantCard {...restraunt.data} key={restraunt.data.id} />
+                    );
+                })
+            }
+        
         </div>
 
         </>
@@ -57,3 +51,26 @@ const Body=()=>{
 };
 
 export default Body;
+
+
+// const BurgerKing={
+//     name:"Burger King",
+//     image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/768px-Burger_King_logo_%281999%29.svg.png",
+//     cusines:["Burger","American"],
+//     rating:"4.2"
+// }
+
+
+// const RestrauntCard=()=>{
+//     return (
+//         <div className="card">
+
+//             <img src={BurgerKing.image}/>
+//             <h2>{BurgerKing.name}</h2>
+//             <h3>{BurgerKing.cusines.join(",   ")}</h3>
+//             <h4>{BurgerKing.rating} stars </h4>
+
+//         </div>
+
+//     );
+// };
