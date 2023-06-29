ffd504e7cae4945994df2cac1c1e4426bf1392c6
SurajSingh7 <220suraj@gmail.com>
2023-06-29 14:01:37 +0530

    Custom Hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,51 +1,14 @@
 import { useParams } from "react-router-dom";
-import { useEffect,useState } from "react";
-import { swiggy_menu_api_URL,ITEM_IMG_CDN_URL } from "../utils/constants";
 import Shimmer from "./Shimmer";
-import { IMG_CDN_URL,RESTAURANT_TYPE_KEY,MENU_ITEM_TYPE_KEY } from "../utils/constants";
+import { IMG_CDN_URL,ITEM_IMG_CDN_URL} from "../utils/constants";
+import useRestaurant from "../utils/useRestaurant";
 
 
 const RestaurantMenu = () => {
     const { resId } = useParams(); 
-    const [restaurant, setRestaurant] = useState(null); 
-    const [menuItems, setMenuItems] = useState([]);
-
-    useEffect(() => {
-      getRestaurantInfo(); 
-    }, []);
+    console.log("render count");
+    const [restaurant,menuItems]=useRestaurant(resId);
   
-    async function getRestaurantInfo() {
-      try {
-        const data = await fetch(swiggy_menu_api_URL + resId);
-        const json = await data.json();
-  
-        // Set restaurant data
-        const restaurantData = json?.data?.cards?.map(x => x.card)?.
-                               find(x => x && x.card['@type'] === RESTAURANT_TYPE_KEY)?.card?.info || null;
-        setRestaurant(restaurantData);
-  
-        // Set menu item data
-        const menuItemsData = json?.data?.cards.find(x=> x.groupedCard)?.groupedCard?.cardGroupMap?.REGULAR?.cards?.
-                              map(x => x.card?.card)?. filter(x=> x['@type'] == MENU_ITEM_TYPE_KEY)?.
-                              map(x=> x.itemCards).flat().map(x=> x.card?.info) || [];
-        
-        const uniqueMenuItems = [];
-        menuItemsData.forEach((item) => {
-          if (!uniqueMenuItems.find(x => x.id === item.id)) {
-            uniqueMenuItems.push(item);
-          }
-        })
-
-        setMenuItems(uniqueMenuItems);
-      } catch (error) {
-        setMenuItems([]);
-        setRestaurant(null);
-        console.log(error);
-      }
-    }
-
-
-
     return !restaurant ? ( <Shimmer />) : (
 
       <div className="restaurant-menu">
